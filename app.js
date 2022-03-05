b5980e2b9abdd54df9e58e43ec173a59a67d531d
Karen Calisaya <karenmcalisaya@gmail.com>
2022-03-05 19:27:46 -0300

    forms

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ http://localhost:${PORT}`));
 app.get('/', (req, res)=>
     res.sendFile(path.join(__dirname, './views/home.html'))
 )
+app.get('/register.html', (req, res)=>
+    res.sendFile(path.join(__dirname, './views/register.html'))
+)
+app.get('/login.html', (req, res)=>
+    res.sendFile(path.join(__dirname, './views/login.html'))
+)
